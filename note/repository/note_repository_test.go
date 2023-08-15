39ee8ad4a6ce96c63bf9c38d7d8bbb60d31cd79e
ujklm23 <ridwanrasyid345@gmail.com>
2023-08-15 19:51:58 +0700

    add: NoteRepository Test

--- note/repository/note_repository_test.go
+++ note/repository/note_repository_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"rdwansch/super_note/domain"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -29,4 +30,72 @@ func TestNoteRepositoryFindAll(t *testing.T) {
 
 	assert.NotEmpty(t, notes)
 	assert.NotNil(t, notes)
+	assert.Len(t, notes, 2)
+}
+
+func TestNoteRepositoryFindById(t *testing.T) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "title", "content", "cover"}).
+		AddRow(1, "My Birthday", "I want a Macbook Pro", "")
+
+	mock.ExpectQuery("SELECT id, title, content, cover FROM notes WHERE id = ?").
+		WithArgs(1).WillReturnRows(rows)
+
+	noteRepository := NewNoteRepository(db)
+	notes, err := noteRepository.FindById(1)
+
+	assert.NoError(t, err)
+	assert.Nil(t, err)
+	assert.Equal(t, "My Birthday", notes.Title)
+}
+
+func TestNoteRepositoryCreate(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+
+	notes := &domain.Note{
+		Title:   "My Birthday",
+		Content: "I want a Macbook Pro",
+		Cover:   "",
+		Author: &domain.Author{
+			Id: 1,
+		},
+	}
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	mock.ExpectPrepare("INSERT INTO notes (title, content, cover, id_user) VALUES (?,?,?,?)").
+		ExpectExec().WithArgs(notes.Title, notes.Content, notes.Cover, notes.Author.Id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	noteRepository := NewNoteRepository(db)
+	err = noteRepository.Create(notes)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, notes.Id)
+}
+
+func TestRepositoryDelete(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	mock.ExpectPrepare("DELETE FROM notes WHERE id = ?").
+		ExpectExec().WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	noteRepository := NewNoteRepository(db)
+	err = noteRepository.Delete(1)
+
+	assert.NoError(t, err)
+	assert.Nil(t, err)
 }
